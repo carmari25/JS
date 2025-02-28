feff0c718ec8ba811e79a3131d8b071b2b19aa77
carmari25 <carmarireyes11@gmail.com>
2025-02-27 20:19:06 -0400

    parte3

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,13 @@
+let a= 5;
+let b=7;
+
+// console.log(a+b, "adicion"); //adicion
+// console.log(a-b), "resta";//resta
+// console.log(a*b, "multiplicacion");//multiplicacion
+// console.log(a/b, "divicion");//division
+// console.log(a%b, "modulo");//modulo
+// console.log(a**b, "potencia");//potencia
+
+//incrementar
+
+
