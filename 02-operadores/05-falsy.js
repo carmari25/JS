88da6280489a899b4ba81f5d5e2b7c305f0a681e
carmari25 <carmarireyes11@gmail.com>
2025-03-03 22:14:23 -0400

    parte5

--- 02-operadores/05-falsy.js
+++ 02-operadores/05-falsy.js
@@ -1,2 +1,43 @@
 //shot-circuit
 
+//False
+//false
+// 0
+// **
+//null
+//undefined
+//NaN
+
+let nombre = '';
+let username = nombre || 'anonimo ';
+console.log(username);
+
+
+let nombre1 = 'carmari';
+let username1 = nombre1 || 'anonimo ';
+console.log(username1);
+
+
+function fn1() {
+    console.log('soy funcion 1')
+    return true;
+}
+
+function fn2() {
+    console.log('soy funcion 2');
+    return true;
+}
+
+let x = fn1() && fn2();
+
+function fn3() {
+    console.log('soy funcion 3')
+    return false;
+}
+
+function fn4() {
+    console.log('soy funcion 4');
+    return true;
+}
+
+let y = fn3() && fn4();
---
+++ 02-operadores/08-ternario.js
@@ -0,0 +1,6 @@
+// expresion ? 'si es true' : 'si es false'
+
+let edad = 23;
+let acceso = edad > 17 ? 'Permitir ingrso' : 'No puede ingresar';
+
+console.log(acceso);
